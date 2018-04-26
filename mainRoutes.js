a8ea72c3ecd779dc53f98d27f0e18c4c2a6b5195
Sizwe Lekoba <lekobas@DLAB63.EIEUG>
2018-04-26 15:17:50 +0200

    small things

--- mainRoutes.js
+++ mainRoutes.js
@@ -2,9 +2,7 @@ let path = require('path');
 let express = require('express');
 let mainRouter = express.Router();
 
-mainRouter.get('/',function (req,res){
-	res.send('Hellow WOrld');
-});
+
 
 
 mainRouter.get('/about',function(req,res){
